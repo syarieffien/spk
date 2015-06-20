2886d56d0a3f616c4314a6b54e4b4df202726203
rizamasta <rizamasta@gmail.com>
2015-06-20 13:44:38 +0700

    update kndidatSeleksi.js

--- app/js/app.js
+++ app/js/app.js
@@ -1473,16 +1473,31 @@ App.controller('kandidatCtrl',["$scope", "$state", "$http", "urlConfig", functio
  */
 App.controller('kandidatSelectCtrl', ["$scope", "$http", "urlConfig", "$state", function($scope,$http,urlConfig,$state){
 
-    $scope.periodes = [{id:'Ganjil',value:'Ganjil'},{id:'Genap',value:'Genap'}];
+    $scope.periodes = [{id:'ganjil',value:'Ganjil'},{id:'genap',value:'Genap'}];
     $scope.periode  = $scope.periodes[0];
     $scope.form     = {};
     $scope.submitProses= function(){
         try{
-            $scope.form.quota = $scope.periode.value;
+            $scope.form.semester    = $scope.periode.id;
+            $scope.form.username    = "rizamasta";
             $http({
                 method      : 'POST',
-                url         : urlConfig.gatewayUrl('kandidat/proses')
+                url         : urlConfig.gatewayUrl('seleksi/proses'),
+                data        : encoding_url($scope.form),
+                headers     : {'Content-Type':'application/x-www-form-urlencoded'}
             })
+                .success(function(response){
+                if(response.message=='success'){
+                    window.alert('Berhasil Memperoses');
+                    window.location.reload();
+                }
+                    else{
+                    window.alert(response.message)
+                }
+            })
+                .error(function(e){
+                    window.alert(e)
+                })
         }
         catch(error){
             $state.go('web.prosesSeleksiKandidat');
